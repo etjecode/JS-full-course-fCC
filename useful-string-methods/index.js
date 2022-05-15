39634f2cc1fa9ffc2cd00e0ba08da67336281701
Feyzan Aydin <feyzan.aydin@gmail.com>
2022-05-16 00:07:54 +0300

    Files updated

--- useful-string-methods/index.js
+++ useful-string-methods/index.js
@@ -1,7 +1,7 @@
 let userName = window.prompt("Choose a username.");
 
-let letter = userName.charAt(0).toUpperCase();
+let newName = (userName.charAt(0).toUpperCase() + userName.slice(1)).replaceAll(" ","");
 
-let newName = letter + userName.slice(1);
+// let newName = letter + userName.slice(1);
 
 document.getElementById("nameLabel").innerHTML = newName;
