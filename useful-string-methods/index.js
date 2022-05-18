9ce65339dd30a2f5e96283d783676228977c1bc2
Feyzan Aydin <feyzan.aydin@gmail.com>
2022-05-18 22:57:01 +0300

    While loop added

--- useful-string-methods/index.js
+++ useful-string-methods/index.js
@@ -1,7 +1,11 @@
-let userName = window.prompt("Choose a username.");
+let userName = "";
+
+while (userName == "") {
+ 
+    userName = window.prompt("Choose a username.");
+
+}
 
 let newName = (userName.charAt(0).toUpperCase() + userName.slice(1)).replaceAll(" ","");
-
-// let newName = letter + userName.slice(1);
 
 document.getElementById("nameLabel").innerHTML = newName;
